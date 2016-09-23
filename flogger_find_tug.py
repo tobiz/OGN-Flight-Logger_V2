6b7e6cb4bb00fb38d7d71173308b9ed66f49074d
tobiz <pjrobinson@metronet.co.uk>
2016-09-24 00:33:20 +0100

    Updates to get better results, not all flights get allocated a tug. Note OGN flight log gets similar errors

--- flogger_find_tug.py
+++ flogger_find_tug.py
@@ -23,10 +23,11 @@ from open_db import opendb
 def find_tug(cursor, db):
     print "find_tug called"
 #    cursor.execute('''SELECT DISTINCT id, stime, duration, registration, max_altitude FROM flight_log_final ''')
-    cursor.execute('''SELECT DISTINCT id, stime, duration, registration, max_altitude FROM flights ''')
+#    cursor.execute('''SELECT DISTINCT id, stime, duration, registration, max_altitude FROM flights ORDER by stime''')
+    cursor.execute('''SELECT id, stime, duration, registration, max_altitude FROM flights ORDER by stime''')
     rows = cursor.fetchall()
     for row in rows:
-        print "Next row candidate for a tug is: ", row, " Type code is: ", settings.FLOGGER_FLEET_LIST[row[3]]
+#        print "Next row candidate for a tug is: ", row, " Type code is: ", settings.FLOGGER_FLEET_LIST[row[3]]
         # row[3] is the aircraft registration. Check whether it's a tug or not
         if settings.FLOGGER_FLEET_LIST[row[3]] > 100 and settings.FLOGGER_FLEET_LIST[row[3]] < 200 :
             # This is a tug flight
@@ -37,11 +38,11 @@ def find_tug(cursor, db):
 #                print "Next row candidate for a glider is: ", flight       
                 if settings.FLOGGER_FLEET_LIST[flight[3]] <= 100:
                     # This is a glider flight
-                    print "Glider flight found: ", flight
+#                    print "Glider flight found: ", flight
                     glider_time = datetime.datetime.strptime("1900/01/01 " + flight[1], '%Y/%m/%d %H:%M:%S')    # Glider takeoff time
                     tdelta_sec = (tug_time - glider_time).total_seconds()      # Difference between 2 times in seconds, note artificially same years, same month, same day
-                    print "Delta flight times is: ", abs(tdelta_sec)
-                    if abs(tdelta_sec) < settings.FLOGGER_DT_TUG_LAUNCH:
+                    if abs(tdelta_sec) < settings.FLOGGER_DT_TUG_LAUNCH:                    
+                        print "Delta flight times is: ", abs(tdelta_sec), " Glider reg: ", flight[3]
                         # Time difference between takeoff times of glider and tug are less than this (secs), hence assume tug launched glider
                         flight_id = flight[0]
                         flight_details = flight
@@ -58,12 +59,13 @@ def find_tug(cursor, db):
             tug_model = cursor.fetchone()
             try:
                 cursor.execute('''UPDATE flights SET tug_registration=?, tug_altitude=?, tug_model=? WHERE id=?''', (row[3], row[4], tug_model[0], flight_id))
-#                print "Tug model added to: ", flight_id, " Model: ", tug_model 
+                print "Tug model added to: ", flight_id, " Model: ", tug_model 
                 db.commit()
             except:
                 print "Failed to add tug model details to flights table for flight: ", flight_details, " tug_model: ", tug_model[0]
         else:
-            print "Not a tug: ", row
+            continue
+#            print "Not a tug: ", row
     return
             
         
