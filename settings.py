b07e7990f1d1d755f463c40c093a1df068763b7a
tobiz <pjrobinson@metronet.co.uk>
2016-11-09 09:23:26 +0000

    Added FLOGGER_TIME_DELTA for phase 2 flight log processing

--- settings.py
+++ settings.py
@@ -63,7 +63,7 @@ FLOGGER_QNH = 0                                               # QNH ie ASL in me
 FLOGGER_LATITUDE, FLOGGER_LONGITUDE = '+54.228833', '-1.209639' # Latitude, longitude of named OGN receiver airfield 
 #FLOGGER_AIRFIELD_DETAILS = ""                                  # Location details for use by geocoder. If blank, "" use LAT, LONG etc
 FLOGGER_AIRFIELD_DETAILS = "Yorkshire Gliding Club UK"          # Location details for use by geocoder. If blank, "" use LAT, LONG etc
-FLOGGER_MIN_FLIGHT_TIME = "0:4:0"                               # hh:mm:ss
+FLOGGER_MIN_FLIGHT_TIME = "0:4:0"                               # Minimum time for duration to be considered a flight, hh:mm:ss
 FLOGGER_KEEPALIVE_TIME = 900                                    # Interval in seconds for sending tcp/ip keep alive on socket connection
 FLOGGER_DB_NAME = "flogger.sql3.2"                              # Name of file for flogger SQLite3 database
 FLOGGER_FLARMNET_DB_URL = "http://www.flarmnet.org/files/data.fln" # URL of Flarmnet database
@@ -73,7 +73,8 @@ FLOGGER_OGN_DB_URL = "http://ddb.glidernet.org/download/?t=1"        # URL of OG
 #FLOGGER_AIRFIELD_NAME = "SuttonBnk"                            # Name of Flarm base station for airfield. NOTE MUST BE PROVIDED
 FLOGGER_AIRFIELD_NAME = "SUTTON BANK"                           # Name of Flarm base station for airfield. NOTE MUST BE PROVIDED AS in flarmdb record
                                                                 # If blank, "" then all aircraft in db are included in logs & tracks
-FLOGGER_FLEET_CHECK = "Y"                                       # Checks Flarm ID is for aircraft fleet of FLOGGER_AIRFIELD_NAME if "Y"
+#FLOGGER_FLEET_CHECK = "Y"                                       # Checks Flarm ID is for aircraft fleet of FLOGGER_AIRFIELD_NAME if "Y"
+FLOGGER_FLEET_CHECK = "N"                                       # Checks Flarm ID is for aircraft fleet of FLOGGER_AIRFIELD_NAME if "Y"
 FLOGGER_QFE_MIN = 100                                            # Minimum altitude in metres attained for inclusion as a flight, ie ~300 ft
 FLOGGER_LOG_PATH =  FLOGGER_BS + "logs"                         # Path where log files are stored 
 FLOGGER_TRACKS = "Y"                                            # If Y flight tracks are recorded. Default is N, ie No tracks logged
@@ -86,7 +87,7 @@ FLOGGER_FLIGHTS_LOG = FLOGGER_BS + ""                           # Folder for csv
 FLOGGER_DATA_RETENTION = 3                                      # Number of days to keep .csv files, ie delete, if "0" keep all files
 FLOGGER_LOG_TUGS = "Y"                                          # Don't log tug flights if "N"
 FLOGGER_TRACKS_IGC = "N"                                        # Dump flight tracks in IGC format if "Y" else no
-FLOGGER_LOG_TIME_DELTA = 0                                      # Number of hours before sunset to start processing flight log
+FLOGGER_LOG_TIME_DELTA = -1                                      # Number of hours before sunset to start processing flight log
 FLOGGER_SMTP_SERVER_URL = ''                                    # URL of smtp server for sending email
 FLOGGER_SMTP_SERVER_PORT = 25                                   # smtp server port number, normally 25
 FLOGGER_SMTP_TX = ""                                            # Flight log sender email addrs
@@ -97,9 +98,11 @@ FLOGGER_TAKEOFF_EMAIL = "Y"                                     # Send email for
 FLOGGER_LANDING_EMAIL = "Y"                                     # Send email for each landing if Yes else no
 FLOGGER_LOG_LAUNCH_FAILURES = "N"                               # Log launch failures, ie below min time & min height
 FLOGGER_LOCATION_HORIZON = '-0:34'                              # Adjustments for angle to horizon for sunset
-FLOGGER_V_TAKEOFF_MIN = 10                                      # Min ground speed considered as takenoff. Same as ogn-live (55Km/h)
-FLOGGER_V_LANDING_MIN = 10                                      # Min ground speed considered as landed. Same as ogn-live (40Km/h)
-FLOGGER_DT_TUG_LAUNCH = 40                                      # Delta t(sec) between glider and tug takeoff times to be tug launched
+FLOGGER_V_TAKEOFF_MIN = 10                                      # Min ground speed considered as takenoff. ogn-live is (55Km/h)
+FLOGGER_V_LANDING_MIN = 10                                      # Min ground speed considered as landed. ogn-live is (40Km/h)
+FLOGGER_DT_TUG_LAUNCH = 20                                      # Delta t(sec) between glider and tug takeoff times to be tug launched
+FLOGGER_DUPLICATE_FLIGHT_DELTA = 120                            # Delta t(sec) between two takeoff times of same aircraft to be different flights
+FLOGGER_TIME_DELTA = "0:2:0"                                    # Time difference between to flights of same aircraft to be considered different flights 
 #
 # The following fields are used to determine if data from APRS is a position packet from any 1 of up to 4 OGN receivers base stations.
 # The OGN receiver areas can overlap and if more then 1 is supplied it will increase the accuracy of both the data and track results
@@ -116,7 +119,7 @@ FLOGGER_DT_TUG_LAUNCH = 40                                      # Delta t(sec) b
 #FLOGGER_APRS_BASE_3 = "UKRUF"         
 #FLOGGER_APRS_BASE_4 = "Linton"
 
-FLOGGER_APRS_BASES = ["SuttonBnk", "UKPOC", "UKRUF", "Linton", "Riponhill", "Wetherby1"]  
+FLOGGER_APRS_BASES = ["SuttonBnk", "UKPOC", "UKRUF", "Linton", "Riponhill"]  
 
 
 # Coded       001-099: Gliders, 
@@ -124,8 +127,8 @@ FLOGGER_APRS_BASES = ["SuttonBnk", "UKPOC", "UKRUF", "Linton", "Riponhill", "Wet
 #             201-299: Motor Gliders, 
 #             301-399: Other
 # Note. No reason for coding these values other than, 'why not!'
-FLOGGER_FLEET_LIST = {"G-BJIV":1, "G-CHEF":2, "G-CHVR":3, "G-CKFN":4, "G-CKJH":5, 
-                      "G-CKLW":6, "G-CJVZ":7, "G-DDKC":8, "G-DDPO":9,  
+FLOGGER_FLEET_LIST = {"G-CHEF":1, "G-CHVR":2, "G-CKFN":3, "G-CKJH":4, 
+                      "G-CKLW":5, "G-CJVZ":6, "G-DDKC":7, "G-DDPO":8,  
                       "G-BETM":101, "G-CIOF":102, "G-MOYR":103, "G-BJIV": 104,
                       "G-OSUT":201, 
                       }
