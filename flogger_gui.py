3d38398aca4e31d2a54a0200c9b419028a086f1b
tobiz <pjrobinson@metronet.co.uk>
2017-02-12 23:57:46 +0000

    Still work in progress, but some progress

--- flogger_gui.py
+++ flogger_gui.py
@@ -4,51 +4,22 @@ import string
 from PyQt4 import QtGui, QtCore, uic
 from PyQt4.Qt import SIGNAL
 import subprocess
-import settings
+#import settings
+import flogger_settings
 from parse import *
 from ConfigParser import *
+from configobj import ConfigObj
+from flogger3 import *
+from flogger_settings import * 
 
-import socket
 
-from libfap import *
-#import flogger_settings
-import string
-import datetime
-import time
-import sqlite3
-import pytz
-from datetime import timedelta
-import sys
-from flarm_db import flarmdb
-from pysqlite2 import dbapi2 as sqlite
-from open_db import opendb 
-import ephem
-from flogger_process_log import process_log
-import argparse
-from flogger_dump_flights import dump_flights
-from flogger_dump_tracks import dump_tracks2
-from flogger_get_coords import get_coords
-from flogger_signals import sig_handler
-import signal
-import os
-import os.path
-from flogger_dump_IGC import dump_IGC
-from flogger_email_log import email_log2
-from flogger_landout import landout_check
-from geopy.distance import vincenty
-from flogger_email_msg import email_msg
-from flogger_find_tug import find_tug
-from flogger_test_YorN import test_YorN
 
 
 # get the directory of this script
 path = os.path.dirname(os.path.abspath(__file__))
 #print("Path: " + path) 
+settings = class_settings()
 
-#form_class = uic.loadUiType("flogger.ui", from_imports, resource_suffix)
-#form_class = uic.loadUiType("flogger.ui")[0]
-#form_class = uic.loadUiType(os.path.join(path,"flogger.ui"))[0]
-#form_class, base_class = uic.loadUiType(os.path.join(path,"flogger.ui"))
 Ui_MainWindow, base_class = uic.loadUiType(os.path.join(path,"flogger.ui"))
 
 #class Window (QtGui.QMainWindow, form_class):
@@ -57,40 +28,116 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+        
 #        self.runFloggerButton.clicked.connect(self.floggerStart) 
         self.actionStart.triggered.connect(self.floggerStart)  
         self.actionStop.triggered.connect(self.floggerStop)  
         self.actionQuit.triggered.connect(self.floggerQuit)  
-        self.AirfieldBaseButton.clicked.connect(self.floggerAirfieldEdit) 
+        self.AirfieldBaseButton.clicked.connect(self.floggerAirfieldEdit)   
+        self.APRSUserButton.clicked.connect(self.floggerAPRSUserEdit)   
+        self.AirfieldDetailsButton.clicked.connect(self.floggerAirfieldDetailsEdit)   
+        self.MinFlightTimeButton.clicked.connect(self.floggerMinFlightTimeEdit)  
+        self.FleetCheckRadioButton.toggled.connect(self.floggerFleetCheckRadioButton) 
         
         # Initialise values from config file
-        self.old_value = self.AirfieldBase.setText(settings.FLOGGER_AIRFIELD_NAME) 
-        self.APRSUser.setText(settings.APRS_USER) 
-        self.APRSPasscode.setText(str(settings.APRS_PASSCODE)) 
-        self.APRSServerHostName.setText(settings.APRS_SERVER_HOST) 
-        self.APRSServerPort.setText(str(settings.APRS_SERVER_PORT))
-        if settings.FLOGGER_LATITUDE[0:1] == "-":
-            latitude = settings.FLOGGER_LATITUDE[1:] + " E"
+
+#        filepath = os.path.join(path, "flogger_settings.py")
+        filepath = os.path.join(path, "flogger_settings_file.txt")
+
+#        filename = open(filepath)
+        try:
+#            config = ConfigObj(filename, encoding='UTF8', raise_errors = True)
+#            self.config = ConfigObj("flogger_settings.py", raise_errors = True)
+            self.config = ConfigObj("flogger_settings_file.txt", raise_errors = True)
+            print "Opened"
+        except:
+            print "Open failed"
+            
+            print self.config
+            
+#
+# This section reads all the values from the config file and outputs these in the gui fields.
+# It also initialises the corresponding settings object config fields. If the values are changed
+# in the gui they must be saved in the config file and used as the current values in the settings object
+#          
+        old_val = self.getOldValue(self.config, "FLOGGER_AIRFIELD_NAME")
+        settings.FLOGGER_AIRFIELD_NAME = old_val
+#        print settings.FLOGGER_AIRFIELD_NAME
+        self.AirfieldBase.setText(old_val)
+         
+        old_val = self.getOldValue(self.config, "APRS_USER")
+        settings.APRS_USER = old_val
+        self.APRSUser.setText(old_val)
+        
+        old_val = self.getOldValue(self.config, "APRS_PASSCODE")    # This might get parsed as an int - need to watch it!
+        settings.APRS_PASSCODE = old_val
+        self.APRSPasscode.setText(old_val)
+        
+        old_val = self.getOldValue(self.config, "APRS_SERVER_HOST")    
+        settings.APRS_SERVER_HOST = old_val
+        self.APRSServerHostName.setText(old_val)
+        
+        old_val = self.getOldValue(self.config, "APRS_SERVER_PORT")    # This might get parsed as an int - need to watch it!
+        settings.APRS_SERVER_PORT = old_val
+        self.APRSServerPort.setText(old_val)
+        
+        old_val = self.getOldValue(self.config, "FLOGGER_AIRFIELD_DETAILS")    
+        settings.FLOGGER_AIRFIELD_DETAILS = old_val
+        self.AirfieldDetails.setText(old_val)
+        
+        old_val = self.getOldValue(self.config, "FLOGGER_MIN_FLIGHT_TIME")    
+        settings.FLOGGER_MIN_FLIGHT_TIME = old_val
+        self.MinFlightTime.setText(old_val)
+        
+        old_val = self.getOldValue(self.config, "FLOGGER_LATITUDE")    # This might get parsed as an real - need to watch it!
+        print "Old_val: " + old_val
+        settings.FLOGGER_LATITUDE = old_val
+        self.AirfieldLatitude.setText(old_val)
+        
+        old_val = self.getOldValue(self.config, "FLOGGER_LONGITUDE")    # This might get parsed as an real - need to watch it!
+        settings.FLOGGER_LONGITUDE = old_val
+        self.AirfieldLongitude.setText(old_val)
+        
+        if settings.FLOGGER_LATITUDE < 0:
+            latitude = str(settings.FLOGGER_LATITUDE)[1:] + " S"
         else:
-            latitude = settings.FLOGGER_LATITUDE[1:] + " W"
-        print "Latitude: " + latitude
-        if settings.FLOGGER_LONGITUDE[0:1] == "-":
-            longitude = settings.FLOGGER_LONGITUDE[1:] + " S"
+            latitude = str(settings.FLOGGER_LATITUDE)[1:] + " N"
+#        print "Latitude: " + latitude
+        
+        if settings.FLOGGER_LONGITUDE < 0:
+            longitude = str(settings.FLOGGER_LONGITUDE)[1:] + " E"
         else:
-            longitude = settings.FLOGGER_LONGITUDE[1:] + " N"
-        print "Latitude: " + longitude
+            longitude = str(settings.FLOGGER_LONGITUDE)[1:] + " W"
+#        print "Latitude: " + longitude
         self.AirfieldLatitude.setText(latitude)
         self.AirfieldLongitude.setText(longitude)
+        
+        old_val = self.getOldValue(self.config, "FLOGGER_FLEET_CHECK")
+        print "Fleet Check: " + old_val 
+        if old_val == "Y":
+            print "Y"
+            self.FleetCheckRadioButton.setChecked()
+        else:
+            print "N"   
+        settings.FLOGGER_FLEET_CHECK = old_val
  
         
     def floggerStart(self):
         print "flogger start"
+        flogger = flogger3()
+        flogger.flogger_start()
 #        flogger_run_string = "flogger Start"   
 #        self.floggerRunBox.setText(settings.FLOGGER_AIRFIELD_NAME)
-        cmd = os.path.join(path,"flogger.py")
-        cmd_args = ["OGNFLOG2", "31134",  "test", "--smt smtp.metronet.co.uk", "--tx pjrobinson@metronet.co.uk", "--rx pjrobinson@metronet.co.uk"]
-        print("cmd_line: " + cmd)
-#        execvp(cmd, cmd_args)
+        cmd = "python " + os.path.join(path,"flogger.py")
+        cmd_line = [cmd, "OGNFLOG2", "31134",  "test", "--smt smtp.metronet.co.uk", "--tx pjrobinson@metronet.co.uk", "--rx pjrobinson@metronet.co.uk"]
+#        print "cmd is: " + cmd
+#        print("cmd_line: ") + str(cmd_line)
+#        os.execvp(cmd, cmd_line[1:])
+     #   os.execv(cmd, "OGNFLOG2", "31134",  "test", "--smt smtp.metronet.co.uk", "--tx pjrobinson@metronet.co.uk", "--rx pjrobinson@metronet.co.uk" )
+#        os.execv("#!flogger.py", ["OGNFLOG2", "31134",  "test", "--smt smtp.metronet.co.uk", "--tx pjrobinson@metronet.co.uk", "--rx pjrobinson@metronet.co.uk"])
+#        os.execlp("/home/pjr/git_neon/OGN-Flight-Logger_V2", "flogger.py","OGNFLOG2", "31134",  "test", "--smt smtp.metronet.co.uk", "--tx pjrobinson@metronet.co.uk", "--rx pjrobinson@metronet.co.uk")
+        exec "flogger.py"
+
 #        execfile("flogger.py", "OGNFLOG2 31134  test --smt smtp.metronet.co.uk --tx pjrobinson@metronet.co.uk --rx pjrobinson@metronet.co.uk")
    
     def floggerStop(self):
@@ -103,53 +150,47 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         print "Base Airfield button clicked" 
         airfield_base = self.AirfieldBase.toPlainText()  
         print "Airfield Base: " + airfield_base
+        self.editConfigField("settings.py", "FLOGGER_AIRFIELD_NAME", airfield_base)
+        airfield_base = self.config["FLOGGER_AIRFIELD_NAME"]
+#        self.AirfieldBase.setText(settings.FLOGGER_AIRFIELD_NAME)
+        self.FLOGGER_AIRFIELD_NAME = airfield_base
+
+    def floggerAPRSUserEdit(self):
+        print "APRS User button clicked" 
+        APRSUser = self.APRSUser.toPlainText()  
+#       print "Airfield B: " + airfield_base
+        self.editConfigField("settings.py", "APRS_USER", APRSUser)
+        APRSUser = self.config["APRS_USER"]
         
-        self.editConfigField("settings.py", "FLOGGER_AIRFIELD_NAME", self.old_value, airfield_base)
-        self.AirfieldBase.setText(settings.FLOGGER_AIRFIELD_NAME)
-
-
-    def editConfigField (self, file_name, field_name, old_value, new_value):
+    def floggerAirfieldDetailsEdit(self):
+        print "Airfield Details button clicked"
+        
+    def floggerMinFlightTimeEdit(self):
+        print "Min Flight Time button clicked"   
+    
+    def floggerFleetCheckRadioButton(self):
+        print "Fleet Check Radio Button clicked" 
+        
+          
+    def editConfigField (self, file_name, field_name, new_value):
         print "editConfig called"
-        file = os.path.join(path, file_name)
-        with open(file, 'rw') as searchfile:
-            searchphrase = field_name
-            for line in searchfile:
-                if searchphrase in line and line[0:1] <> "#":
-#                    print "Line is: " + line
-                    pos = line.find("=")
-#                    print "Position: " + str(pos)
-                    line_list = line.split()
-#                    print line_list
-                    parse_str1 = field_name + " = " + "{field_val1}" + "#" + "{field_val2}"
-#                    print "Parse string1: " + parse_str1
-                    res1 = parse(parse_str1, line)
-                    print res1
-                    print "Field1: " + res1["field_val1"] + ", " + "Field2: " + res1["field_val2"]
-#                    print "Field1 Type: " +  str(type(res1["field_val1"])) + ", " + "Field2 Type: " + str(type(res1["field_val2"]))
-                    
-                    field1_type = str(type(res1["field_val1"]).__name__)
-                    field2_type = str(type(res1["field_val2"]).__name__)
-#                    print "Field1 Type: " +  type(res1["field_val1"]) + ", " + "Field2 Type: " + type(res1["field_val2"])
-                    print "Field1 Type: " +  field1_type + ", " + "Field2 Type: " + field2_type
-                    if field1_type == "str":
-                        print True
-                    else:
-                        print False
-                    print "Line is: " + line
-                    new_line = line.replace(self.old_value, new_value, 1)
-#                    new_line = field_name + " = " + new_value + " # " + res1["field_val2"]
-                    print new_line
-                    
-#                    import fileinput
-#                    parse_line = fileinput.input(files, inplace = 1)
-                    # Does a list of files, and
-                    # redirects STDOUT to the file in question
-                    #for line in fileinput.input(files, inplace = 1):
-#                    print line.replace(self.old_value, new_value),
+        self.config[field_name] = new_value
+        self.config.write()
 
-                    break
             
-            
+    def setOldValue(self, config_field_name): 
+#        val = self.config[config_field_name]
+        val = settings.config[config_field_name]
+        setattr(self, config_field_name, val) #equivalent to: self.varname= 'something'
+#        settings.config_field_name = val
+        return self.config[config_field_name]
+    
+    def getOldValue(self, config, config_field_name): 
+        val = config[config_field_name]
+        setattr(self, config_field_name, val)
+        return config[config_field_name]
+        
+               
             
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
@@ -158,94 +199,4 @@ if __name__ == "__main__":
     sys.exit(app.exec_())
 
 
-#class MainWindow (QtGui.QMainWindow, form_class):
-#    def __init__(self):
-#        QtGui.QMainWindow.__init__(self)
-#        form_class.__init__(self)
-#        self = MainWindow()
-#        self.setupUi(self)
-        
-
-#        flogger_ui = path + "/flogger.ui"
-#        print("flogger_ui: " + flogger_ui)
-                                  
-#        self = uic.loadUiType(flogger_ui)[0]
-#        self.setupUi(self)
-#        super(Window, self).__init__()
-#        self.setGeometry(50, 50, 500, 300)
-#        self.setWindowTitle("PyQT Flogger")
-#        self.setWindowIcon(QtGui.QIcon(""))
-#        self.connect(self.actionStart, SIGNAL("activated()"), self.floggerStart)
-#        print "Init"
-#        self.connect(self.actionStart, QtCore.SIGNAL("activated()"),self.floggerStart)
-#        self.ui.connect(actionStart, QtCore.SIGNAL('activated()'), self, QtCore.SLOT('trigger()'))
-#              self.connect(self.ui.actionOpen, QtCore.SIGNAL('triggered()'), self, QtCore.SLOT('open()'))
-#        actionStart.signal.connect(slot_function)
-#        self.pushButton.clicked.connect(self.floggerStart)
-#        print "actionStart connect"
-#        self.home()
-#        self.show()
-#    @QtCore.pyqtSlot()
-#
-#    def floggerStart(self):
-#        self.n = self.n + 1
-#        print("flogger start called ")
-#        return
-#        price = int(self.floggerRunBox.toPlainText())
-#        tax = (self.tax_rate.value())
-#        total_price = price  + ((tax / 100) * price)
-#        flogger_run_string = "flogger Start"
-#        self.floggerRunBox.setText(flogger_run_string)
-        
-#        /*price = int(self.price_box.toPlainText())
-#        tax = (self.tax_rate.value())
-#        total_price = price  + ((tax / 100) * price)
-#        total_price_string = "The total price with tax is: " + str(total_price)
-#        self.results_window.setText(total_price_string)
-#        */
-        
-#    def home(self): 
-#        exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)        
-#        exitAction.setShortcut('Ctrl+Q')
-#        exitAction.setStatusTip('Exit application')
-#        exitAction.triggered.connect(QtGui.qApp.quit)
-
-#        self.statusBar()
-#        menubar = self.menuBar()
-#        fileMenu = menubar.addMenu('&File')
-#        fileMenu.addAction(exitAction)
-        
-#        configMenu = menubar.addMenu('&Configure')
-#        configMenu.addAction(exitAction)
-        
-#        self.setGeometry(300, 300, 300, 200)
-#        self.setWindowTitle('PyQt Flogger')
-#        btn = QtGui.QPushButton("Quit", self)    
-#        btn.clicked.connect(self.close_app)
-#        btn.resize(100,100)
-#        btn.move(100,100)
-#        self.connect(self.actionStart, SIGNAL("clicked()"),self.floggerStart)
-#        self.n = 0
-#        self.show()
-        
-#    def close_app(self):
-#        print("Exit flogger")
-#        sys.exit()
-        
-#    def actionStart(self):
-#        print("actionStart called")
-#        sys.exit()
-        
-#def main():
-#    app = QtGui.QApplication(sys.argv)
-#    GUI = Window()
-#    sys.exit(app.exec_())
-    
-#main()
-
-#if __name__ == "__main__":
-#    app = QtGui.QApplication(sys.argv)
-#    window = MainWindow()
-#    window.show()
-#    sys.exit(app.exec_())
     
